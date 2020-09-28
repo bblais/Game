b451245f491fc25e50c02f31eac5313e3607a131
Brian Blais <bblais@gmail.com>
2020-09-28 13:13:22 -0400

    added storage

--- Game/game.py
+++ Game/game.py
@@ -11,8 +11,44 @@ import os
 from random import randint
 
 
+class Storage(object):
+    def __init__(self):
+        self.data=[]
+    
+    def __add__(self,other):
+        s=Storage()
+        s+=other
+        return s
+        
+    def __iadd__(self,other):
+        self.append(*other)
+        return self
+        
+    def append(self,*args):
+        if not self.data:
+            for arg in args:
+                self.data.append([arg])
 
- 
+        else:
+            for d,a in zip(self.data,args):
+                d.append(a)
+       
+    def arrays(self):
+        from numpy import array
+        for i in range(len(self.data)):
+            self.data[i]=array(self.data[i])
+
+        ret=tuple(self.data)
+        if len(ret)==1:
+            return ret[0]
+        else:
+            return ret
+
+    def __array__(self):
+        from numpy import vstack
+        return vstack(self.arrays())
+
+
 def sortedby(L,values,reverse=False):
 
     data=list(zip(values,L))
